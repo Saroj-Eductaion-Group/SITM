17a275b4ff7d2f6e63eb4f8b20f6daad33adc24f
vkassingh <44vikasrajput44@gmail.com>
2025-09-29 17:03:26 +0530

    Removed floating admission banner

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,19 +1,17 @@
 import AboutSITM from "./AboutSection";
 import Chatbot from "./ChatBot";
 import CompaniesSlider from "./CompanyTieUps";
-import FloatingAdmissionBanner from "./FloatingTimer";
 
 import InstagramFeed from "./InstagramFeed";
 import MainSection from "./MainSection";
 import PlacementSection from "./PlacementSection";
 import SitmCourses from "./SitmCourses";
-import TopInfoBar from "./TopInfoBar";
 
 export default function Home() {
   return (
     <>
 
-    <FloatingAdmissionBanner />
+   
     <MainSection />
     <Chatbot/>
     <AboutSITM/>
